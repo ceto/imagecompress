6ef5e69d939e0d22bf7b679beaaaa30760f1fbda
Gábor Szabó <szabogabi@gmail.com>
2021-10-06 12:50:45 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,40 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var imageminPngquant = require('imagemin-pngquant');
 
+
+// fetch command line arguments
+const arg = (argList => {
+
+  let arg = {}, a, opt, thisOpt, curOpt;
+  for (a = 0; a < argList.length; a++) {
+
+    thisOpt = argList[a].trim();
+    opt = thisOpt.replace(/^\-+/, '');
+
+    if (opt === thisOpt) {
+
+      // argument value
+      if (curOpt) arg[curOpt] = opt;
+      curOpt = null;
+
+    }
+    else {
+
+      // argument name
+      curOpt = opt;
+      arg[curOpt] = true;
+
+    }
+
+  }
+
+  return arg;
+
+})(process.argv);
 // ### Images
 // `gulp images` - Run lossless compression on all the images.
 gulp.task('images', function() {
-    return gulp.src('original/**/*')
+    return gulp.src(arg.dir + '/**/*')
         .pipe(
             imagemin([
                 imagemin.mozjpeg({
@@ -26,5 +56,5 @@ gulp.task('images', function() {
 				verbose: true
 			})
         )
-        .pipe(gulp.dest('compressed'));
+        .pipe(gulp.dest(arg.dir));
 });
